9063fbb1f6d48398971e8a45cd788bfd3998e9a0
Seongminlee812 <sungminlee32@gmail.com>
2024-02-07 12:23:07 +0900

    2024-02-07

--- codetree/BackTracking/겹치지_않게_선분_고르기.py
+++ codetree/BackTracking/겹치지_않게_선분_고르기.py
@@ -4,48 +4,45 @@ for _ in range(n):
     a, b = map(int, input().split())
     lines.append((a, b))
 
-visited = [False] * 1001
-
 # 체크로직 변경 필요!
-def check(line):
+def overlapped(line1, line2):
+    (ax1, ax2), (bx1, bx2) = line1, line2
+
+    return (ax1 <= bx1 and ax2 >= bx1) or (ax1 >= bx1 and bx2 >= ax1) or \
+        (ax2 >= bx2 and ax1 <= bx2) or (ax2 <= bx2 and bx1 <= ax2)
+
+def check():
     """ 사용 가능한 선분인지 체크하는 함수"""
-    x1, x2 = line[0], line[1]
-    for i in range(x1, x2 + 1):
-        if visited[i]:
-            return False
+    for i, line1 in enumerate(selected):
+        for j, line2 in enumerate(selected):
+            if i > j and overlapped(line1, line2):
+                return False
     return True
 
-def use(line):
-    """해당 선분을 사용하는 함수 : visited를 체크할 수 있게"""
-    global visited, now
-    x1, x2 = line[0], line[1]
-    # visited[x1], visited[x2] = [True], [True]
-    visited[x1:x2 + 1] = [True] * (x2 - x1 + 1)
-    now.append((x1, x2))
-    pass
-
-def pop():
-    global visited, now
-    x1, x2 = now.pop()
-    # visited[x1], visited[x2] = [False], [False]
-    visited[x1:x2 + 1] = [False] * (x2 - x1 + 1)
-
-now = []
 ans = 0
+selected = []
+
 def choose(index):
-    global now, ans
-    if index >= len(lines):
-        ans = max(ans, len(now))
+    global ans
+    if index == len(lines):
+        print(selected)
+        if check():
+            ans = max(ans, len(selected))
         return
 
-    for i in range(index, len(lines)):
-        if check(lines[i]):
-            use(lines[i])
-            choose(index + 1)
-            pop()
-            choose(index + 1)
-        else:
-            choose(index + 1)
+    selected.append(lines[index])
+    choose(index + 1)
+    selected.pop()
+    choose(index + 1)
 
 choose(0)
 print(ans)
+
+"""test_case
+
+3
+1 2
+1 4
+3 4
+
+"""
